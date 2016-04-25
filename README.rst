18f6a21458e7d379e1e9bf5221eb3cc2df7c76bd
Satyam <satyam.gupta2@gmail.com>
2016-04-26 01:58:54 +0530

    Major changes in cli.py and minor in others

--- README.rst
+++ README.rst
@@ -17,8 +17,3 @@ If you've cloned this project, and want to install the library (*and all
 development dependencies*), the command you'll want to run is::
 
     $ pip install -e .[test]
-
-If you'd like to run all tests for this project, you would run the following command::
-
-    $ python setup.py test
-
--- figletgw/cli.py
+++ figletgw/cli.py
@@ -1,29 +1,52 @@
 """
-skele
+figletgw
 
 Usage:
-  skele hello
-  skele -h | --help
-  skele --version
+  figlet [-f | --font <font_name>] <text>
+  figlet -h | --help
+  figlet --version
+  figlet -l | --list-font
 
 Options:
   -h --help                         Show this screen.
   --version                         Show version.
+  -l --list-font                  List supported fonts
+  -f --font                       Font to be used for formatting (default='small')
 
 Examples:
-  skele hello
+  figletgw -f colossal "hello"
+  figlet -l
 
 Help:
   For help using this tool, please open an issue on the Github repository:
-  https://github.com/rdegges/skele-cli
+  https://github.com/satyam-gupta/figletgw
 """
 
 
 from inspect import getmembers, isclass
-
 from docopt import docopt
+import socket, sys, re
 
 from . import __version__ as VERSION
+
+def call_figlet(param):
+
+    port = 70
+    host = 'gopher.floodgap.com'
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+
+    sock.send('fun/figletgw\t' +param+ '\r\n')
+    result = ""
+
+    while 1:
+        buf = sock.recv(2048)
+        if not len(buf):
+          break
+        result += buf
+
+    return result
 
 
 def main():
@@ -31,12 +54,24 @@ def main():
     import commands
     options = docopt(__doc__, version=VERSION)
 
-    # Here we'll try to dynamically match the command the user is trying to run
-    # with a pre-defined command class we've already created.
-    for k, v in options.iteritems():
-        if hasattr(commands, k) and v:
-            module = getattr(commands, k)
-            commands = getmembers(module, isclass)
-            command = [command[1] for command in commands if command[0] != 'Base'][0]
-            command = command(options)
-            command.run()
+    if(options['--list-font']):
+      
+        result = call_figlet("test")
+        fonts = re.findall(r'(?<=font=)\w+',result)
+        print ' '.join(fonts)
+
+    elif(options['<text>']):
+      
+        param = options['<text>']+"|font="
+        
+        if(options['--font']):
+            param += options['<font_name>']
+        else:
+            param += "small"
+
+        result = call_figlet(param)
+
+        if("Here is the result" in result):
+            print result[117:]
+        else:
+            print result
